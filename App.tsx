e448b33ef2247fb265f58c8d40de4963bac01719
renmurakami <autoeyy@gmail.com>
2022-05-20 11:52:54 +0900

    hide header

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ export default function App() {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator initialRouteName="Home">
-				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
 				<Stack.Screen
 					name="User"
 					component={UserScreen}
--- src/components/homeScreen.tsx
+++ src/components/homeScreen.tsx
@@ -1,13 +1,14 @@
 import { VFC } from "react";
 import { Button, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { HomeScreenProps } from "../types/rootType";
 
 const HomeScreen: VFC<HomeScreenProps> = ({ navigation, route }) => {
 	return (
-		<View>
+		<SafeAreaView>
 			<Text>ホーム画面</Text>
 			<Button title="ユーザー" onPress={() => navigation.navigate("User", { userId: "1" })} />
-		</View>
+		</SafeAreaView>
 	);
 };
 
