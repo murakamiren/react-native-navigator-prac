83666c3223d1870f8b9c9e4dad11aa9778be250a
renmurakami <autoeyy@gmail.com>
2022-05-20 11:21:35 +0900

    initial commit

---
+++ src/components/homeScreen.tsx
@@ -0,0 +1,12 @@
+import { VFC } from "react";
+import { Text, View } from "react-native";
+
+const HomeScreen: VFC = () => {
+	return (
+		<View>
+			<Text>home</Text>
+		</View>
+	);
+};
+
+export default HomeScreen;
